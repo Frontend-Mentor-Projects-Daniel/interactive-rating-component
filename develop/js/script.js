8e21e8efd66ebee8ab37effac91028c7021aac04
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-07-29 10:51:36 -0400

    add functionality for storing and displaying rating user selected

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,18 +1,30 @@
-// SUDO CODE
-
-// High Level
-// when the submit button is clicked on, the user will be redirected to the thankyou.html page
-// on thankyou page, show what user has selected
-
-// Low Level
-// add event listener to button -> onClick -> window.location.replace("thankyou.html");
-//
-
 // GLOBAL VARIABLES
-const btnOrange = document.querySelector('[data-btn--orange]');
+const btnOrange = document.querySelector('[data-btn--orange]') || null;
+const ratings = document.querySelectorAll('[data-rating]') || null;
+const userRating = document.querySelector('[data-rating-selection]') || null;
+
+// PROCEDURAL CODE
+ratings.forEach((rating) => {
+  rating.addEventListener('click', () => {
+    if (rating.firstElementChild.checked) {
+      const jsonRating = JSON.stringify(
+        rating.firstElementChild.nextElementSibling.innerText
+      );
+
+      localStorage.setItem('rating', jsonRating);
+    }
+  });
+});
+
+if (userRating != null) {
+  let parsedRating = JSON.parse(localStorage.getItem('rating'));
+  userRating.textContent = parsedRating;
+}
 
 // EVENT LISTENERS
-btnOrange.addEventListener('click', handleSubmit);
+if (btnOrange) {
+  btnOrange.addEventListener('click', handleSubmit);
+}
 
 // FUNCTIONS
 function handleSubmit() {
